872b446a3ac13d59b6137052eb645bfe719dd793
mohamaed-mahmoud23 <mohmaed-mahmoud23@gmail.com>
2025-02-19 00:30:44 +0000

    أضفت تعديلات على ملف ExperienceTopMiddle

--- src/components/contactMeSection/ContactMeRight.jsx
+++ src/components/contactMeSection/ContactMeRight.jsx
@@ -5,14 +5,15 @@ const ContactMeRight = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
       <img
-        src="../../public/images/email-image.png"
+        src="/images/email-image.png"
         alt="email image"
         className="max-w-[300px]"
       />
+
       <ContactInfo />
       <ContactSocial />
     </div>
   );
-};  
+};
 
 export default ContactMeRight;
